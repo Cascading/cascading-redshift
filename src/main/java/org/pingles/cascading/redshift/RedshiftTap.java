afe7fe2a2c1e929debb8974f25176d5b45ca7634
Paul Lam <paul@quantisan.com>
2013-05-22 10:04:21 +0100

    add docstring and SinkMode.UPDATE to not drop table

--- src/main/java/org/pingles/cascading/redshift/RedshiftTap.java
+++ src/main/java/org/pingles/cascading/redshift/RedshiftTap.java
@@ -22,8 +22,40 @@ public class RedshiftTap extends Hfs {
     private final String s3SecretKey;
     private final String jdbcUrl;
     private final String id = UUID.randomUUID().toString();
+    private final SinkMode sinkMode;
 
+    /**
+     * Redshift sink-only tap to stage data to S3 and then issue a JDBC COPY command to specified Redshift table.
+     * Drops existing table by default.
+     *
+     * To choose {@link SinkMode}, use {@link #RedshiftTap(String, String, String, String, String, String, RedshiftScheme, cascading.tap.SinkMode)}
+     *
+     * @param s3Uri uri of s3 staging directory
+     * @param s3AccessKey aws access key
+     * @param s3SecretKey aws secret key
+     * @param jdbcUrl Redshift jdbc url
+     * @param username Redshift user name
+     * @param password Redshift password
+     * @param scheme scheme object
+     */
     public RedshiftTap(String s3Uri, String s3AccessKey, String s3SecretKey, String jdbcUrl, String username, String password, RedshiftScheme scheme) {
+        this(s3Uri, s3AccessKey, s3SecretKey, jdbcUrl, username, password, scheme, SinkMode.REPLACE);
+    }
+
+    /**
+     * Redshift sink-only tap to stage data to S3 and then issue a JDBC COPY command to specified Redshift table
+     *
+     * @param s3Uri uri of s3 staging directory
+     * @param s3AccessKey aws access key
+     * @param s3SecretKey aws secret key
+     * @param jdbcUrl Redshift jdbc url
+     * @param username Redshift user name
+     * @param password Redshift password
+     * @param scheme scheme object
+     * @param sinkMode use {@link SinkMode#REPLACE} to drop Redshift table before loading;
+     *                 {@link SinkMode#UPDATE} to not drop table for incremental loading
+     */
+    public RedshiftTap(String s3Uri, String s3AccessKey, String s3SecretKey, String jdbcUrl, String username, String password, RedshiftScheme scheme, SinkMode sinkMode) {
         super(new TextDelimited(scheme.getSinkFields()), s3Uri, SinkMode.REPLACE);
         this.s3Uri = s3Uri;
         this.s3AccessKey = s3AccessKey;
@@ -32,6 +64,7 @@ public class RedshiftTap extends Hfs {
         this.username = username;
         this.password = password;
         this.scheme = scheme;
+        this.sinkMode = sinkMode;
     }
 
     @Override
@@ -40,8 +73,10 @@ public class RedshiftTap extends Hfs {
         RedshiftJdbcClient client = new RedshiftJdbcClient(jdbcUrl, username, password);
         try {
             client.connect();
-            client.execute(scheme.buildDropTableCommand());
-            client.execute(scheme.buildCreateTableCommand());
+            if (this.sinkMode == SinkMode.REPLACE) {
+                client.execute(scheme.buildDropTableCommand());
+                client.execute(scheme.buildCreateTableCommand());  // TODO how to create table check if not exist?
+            }
             client.execute(scheme.buildCopyFromS3Command(s3Uri, s3AccessKey, s3SecretKey));
         } catch (ClassNotFoundException e) {
             LOGGER.error("Couldn't commit to Redshift", e);
